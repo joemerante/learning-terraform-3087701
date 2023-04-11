8479cebe7a44ea280cc3ff4b56e8bd084cf6ef90
Joe Merante <merante.joe@gmail.com>
2023-04-11 17:27:59 -0400

    try the linter

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "instance_type" {
- description = "Type of EC2 instance to provision"
- default     = "t3.nano"
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
 }
 
 variable "ami_filter" {
@@ -12,8 +12,8 @@ variable "ami_filter" {
   })
 
   default = {
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-    owner  = "979382823631" # Bitnami
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
@@ -21,22 +21,22 @@ variable "environment" {
   description = "Development environment"
 
   type = object({
-    name = string
+    name           = string
     network_prefix = string
   })
 
   default = {
-    name = "dev"
+    name           = "dev"
     network_prefix = "10.0"
   }
 }
 
 variable "asg_min_size" {
   description = "Minimum number of instances in the ASG"
-  default = 1
+  default     = 1
 }
 
 variable "asg_max_size" {
   description = "Maximum number of instances in the ASG"
-  default = 1
+  default     = 1
 }
